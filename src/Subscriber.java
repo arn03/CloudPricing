5e258ad5891c5905829676769c1277bdb570b9f8
arn03 <58330407+arn03@users.noreply.github.com>
2020-10-05 18:07:58 +0200

    version before random creation of instances

--- src/Subscriber.java
+++ src/Subscriber.java
@@ -3,4 +3,25 @@ public class Subscriber {
 	private double aprioriDemand[];// 
 	private double v; //coeficcien of willingness to change habits
 	private int lr;// level of service required
+	
+	
+	
+	public Subscriber(double[] aprioriDemand, double v, int lr) {
+		super();
+		this.aprioriDemand = aprioriDemand;
+		this.v = v;
+		this.lr = lr;
+	}
+	
+	public double[] getAprioriDemand() {
+		return aprioriDemand;
+	}
+	public double getV() {
+		return v;
+	}
+	public int getLr() {
+		return lr;
+	}
+	
+	
 }
--- src/User.java
+++ src/User.java
@@ -5,4 +5,31 @@ public class User {
 	private int rd;//release date
 	private int dd;//due date
 	private int duration;//duration
+	
+	
+	
+	public User(int lr, double opt, int rd, int dd, int duration) {
+		super();
+		this.lr = lr;
+		this.opt = opt;
+		this.rd = rd;
+		this.dd = dd;
+		this.duration = duration;
+	}
+	public int getLr() {
+		return lr;
+	}
+	public double getOpt() {
+		return opt;
+	}
+	public int getRd() {
+		return rd;
+	}
+	public int getDd() {
+		return dd;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	
 }
